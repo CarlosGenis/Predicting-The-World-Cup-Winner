dbecc9f766fcbfc4d60eea7de7a5cee363ce3ea3
CarlosGenis <carlosgenis06@gmail.com>
2022-11-14 19:40:11 -0500

    Filtered df by comp name

--- clean_data.py
+++ clean_data.py
@@ -1,4 +1,6 @@
 import pandas as pd
 
 df = pd.read_csv ('results.csv')
-print(df)
+#The data set below is for the purposes of getting data for countries that haven't been to the World Cup recentely
+df_filtered_comp = df[(df['tournament'] == 'UEFA Euros') | (df['tournament'] == 'AFC Asian Cup') | (df['tournament'] == 'Gold Cup')]
+print(df_filtered_comp)
