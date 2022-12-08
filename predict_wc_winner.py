6d9ed451c4e907368abe327500aacac18485ed81
CarlosGenis <carlosgenis06@gmail.com>
2022-12-07 23:58:01 -0500

    Update predict_wc_winner.py
    Added dummies to dataset plus wrote a function to replace NaN values with that column's mean

--- predict_wc_winner.py
+++ predict_wc_winner.py
@@ -16,7 +16,26 @@ df['date'] = pd.to_datetime(df['date'], format = '%m/%d/%Y')
 #df.hist(bins=50, figsize=(20,15))
 #plt.show()
 
-train_set, test_split = train_test_split(df, test_size = 0.2, random_state=42)
-#print(train_set)
-#print(test_split)
-reg = LinearRegression().fit(train_set, test_split)
+#this code is forthe purpose of one-hot encoding
+#test = pd.get_dummies(df, drop_first=True)
+#print(test)
+#test.to_csv('international_matches_data_w_dummies.csv')
+
+#function to get mean of a column and fills in Nan values
+
+def get_mean_and_fill_NaN(df, col):
+    mean_value=df[col].mean()
+    df[col].fillna(value=mean_value, inplace=True)
+
+
+#using our new df
+df_w_dummies = pd.read_csv('international_matches_data_w_dummies.csv')
+df_w_dummies.drop(columns=df.columns[0], axis=1, inplace=True)
+
+#call get_mean_and_fill_NaN function
+get_mean_and_fill_NaN(df_w_dummies, 'home_team_goalkeeper_score') 
+test = df_w_dummies['home_team_goalkeeper_score']
+#train_set, test_split = train_test_split(df_w_dummies, test_size = 0.2, random_state=42)
+#test = train_set
+#test2 = test_split
+
